e4f1a480cebee567d25e864e8e8a89a7769ddb15
Daniel Blanco <74727652+dabadrian@users.noreply.github.com>
2023-07-13 23:54:58 -0400

    Add files via upload

---
+++ src/components/MenuComponente.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="menu">
+      <ul>
+        <li v-for="item in menuItems" :key="item.id">{{ item.label }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        menuItems: [
+          { id: 1, label: 'Inicio' },
+          { id: 2, label: 'Productos' },
+          { id: 3, label: 'Acerca de' },
+          { id: 4, label: 'Contacto' }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  .menu {
+    background-color: #f2f2f2;
+    padding: 10px;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin-right: 10px;
+    cursor: pointer;
+  }
+  </style>
---
+++ src/components/PiePagina.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer class="footer">
+      <p>{{ copyright }}</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        year: new Date().getFullYear(),
+        companyName: 'Mi Empresa'
+      };
+    },
+    computed: {
+      copyright() {
+        return `© ${this.year} ${this.companyName}`;
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .footer {
+    background-color: #f2f2f2;
+    padding: 10px;
+    text-align: center;
+  }
+  </style>
